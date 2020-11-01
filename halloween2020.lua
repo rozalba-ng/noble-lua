5e78e2c110b36a625ef23acb0cb0ad141425ddd3
AcoStar <greenshiftkaito@hotmail.com>
2020-11-01 15:14:22 +0300

    Bug fixed

--- halloween2020.lua
+++ halloween2020.lua
@@ -73,7 +73,6 @@ local allowed_area_sphere = {
 	radius = 55,
 }
 local function AllowedArea_StartQuest( _,_,_, player )
-	player:SendBroadcastMessage("Проверка")
 	if player:GetMapId() == 9001 and player:GetData("Halloween2020Stage") == 0 then
 		player:RegisterEvent( AllowedArea_StartQuest, 5000, 1 )
 		if player:GetDistance( allowed_area_sphere.x, allowed_area_sphere.y, allowed_area_sphere.z ) > allowed_area_sphere.radius then
