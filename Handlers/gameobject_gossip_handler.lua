ba579c312c243200534194da296856fef70888e7
Саша <rozalba89@gmail.com>
2020-11-03 13:56:40 +0300

    gobsize user

--- Handlers/gameobject_gossip_handler.lua
+++ Handlers/gameobject_gossip_handler.lua
@@ -337,7 +337,7 @@ function GoMovable.OnGossipSelectGoMovable(event, player, object, sender, intid,
 				PlayerBuild.targetgobject[player:GetGUIDLow()] = guid;
 				local result = num/100;
 				print(gob);
-				gob:SetScale(result);
+				gob:SetScale(1.2)
 				print(result)
 				print("UPDATE gameobject SET custom_scale = " .. result .. " WHERE GUID = " .. guidLow);
 				WorldDBQuery("UPDATE gameobject SET custom_scale = " .. result .. " WHERE GUID = " .. guidLow);
