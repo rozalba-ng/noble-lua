a017ce14153bf07dd3f42372ed95ad516d010ef5
AcoStar <greenshiftkaito@hotmail.com>
2020-12-12 18:52:39 +0300

    Смотрим за работой пакетов

--- Stormwind system/hidden_role_aura.lua
+++ Stormwind system/hidden_role_aura.lua
@@ -2,14 +2,8 @@
 local entry_aura = 91062
 --	Когда аура 
 local function AuraCancelled( event, packet, player )
-	print("== NEW PACKET ==")
-	print(event)
-	print( packet:GetOpcode() )
-	print("== END ==")
+	if player then player:SendBroadcastMessage("123") end
 end
--- Когда игрок отменил себе ауру
-RegisterPacketEvent( 0x136, 5, AuraCancelled ) -- PACKET_EVENT_ON_PACKET_RECEIVE, 0x136 - CMSG_CANCEL_AURA
-RegisterPacketEvent( 0x136, 7, AuraCancelled ) -- PACKET_EVENT_ON_PACKET_SEND, 0x136 - CMSG_CANCEL_AURA -- ВЕРОЯТНО НЕ НУЖНО
 -- Когда сервер обновил ауру
-RegisterPacketEvent( 0x496, 5, AuraCancelled ) -- PACKET_EVENT_ON_PACKET_RECEIVE, 0x136 - SMSG_AURA_UPDATE -- ВЕРОЯТНО НЕ НУЖНО
+--RegisterPacketEvent( 0x496, 5, AuraCancelled ) -- PACKET_EVENT_ON_PACKET_RECEIVE, 0x136 - SMSG_AURA_UPDATE -- ВЕРОЯТНО НЕ НУЖНО
 RegisterPacketEvent( 0x496, 7, AuraCancelled ) -- PACKET_EVENT_ON_PACKET_SEND, 0x136 - SMSG_AURA_UPDATE
