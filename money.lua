e06c1b67c8c5701f527e06a4ec3310d83c754f31
AcoStar <greenshiftkaito@hotmail.com>
2020-12-13 19:45:08 +0300

    Смена кодировки

--- money.lua
+++ money.lua
@@ -1,7 +1,7 @@
 
---	NPC заменяющий зону Штормграда (Если есть в радиусе 25 ярдов)
+--	NPC Р·Р°РјРµРЅСЏСЋС‰РёР№ Р·РѕРЅСѓ РЁС‚РѕСЂРјРіСЂР°РґР° (Р•СЃР»Рё РµСЃС‚СЊ РІ СЂР°РґРёСѓСЃРµ 25 СЏСЂРґРѕРІ)
 local entry_npc = 1211001
---	NPC создающий зону с бонусом (Для общесерв ивентов)
+--	NPC СЃРѕР·РґР°СЋС‰РёР№ Р·РѕРЅСѓ СЃ Р±РѕРЅСѓСЃРѕРј (Р”Р»СЏ РѕР±С‰РµСЃРµСЂРІ РёРІРµРЅС‚РѕРІ)
 local entry_bonus_npc = 1211002
 
 --[[Every 15 minutes player recieve 10 copper. If pleyer is in guild, he auto-deposit 10 copper to guild, but resieve 5 additional copper]]
@@ -9,44 +9,44 @@ local function calculateMoney()
 	local onlinePlayers = GetPlayersInWorld( 2 ); --[[ 2-neutral, both horde and aliance]]		
 	for _, player in ipairs( onlinePlayers ) do	
 		if ( player:IsAFK() == false ) then
-		--	Добавление денег
+		--	Р”РѕР±Р°РІР»РµРЅРёРµ РґРµРЅРµРі
 			player:ModifyMoney( 40 );
 			local guild = player:GetGuild();
 			if ( guild ~= nil ) then		
 				player:ModifyMoney( -5 );
 				guild:DepositBankMoney( player, 15 )
 			end
-		--	Добавление репутации
+		--	Р”РѕР±Р°РІР»РµРЅРёРµ СЂРµРїСѓС‚Р°С†РёРё
 			if SocialTime() then
 				local f
 				if player:GetQuestStatus( 110052 ) == 6 then
-				--	Игрок выполнил квест на вступление в Тени Штормграда
+				--	РРіСЂРѕРє РІС‹РїРѕР»РЅРёР» РєРІРµСЃС‚ РЅР° РІСЃС‚СѓРїР»РµРЅРёРµ РІ РўРµРЅРё РЁС‚РѕСЂРјРіСЂР°РґР°
 					f = thiefs_faction
 				elseif player:GetQuestStatus( 110053 ) == 6 then
-				--	Игрок выполнил квест на вступление в Королевство Штормград
+				--	РРіСЂРѕРє РІС‹РїРѕР»РЅРёР» РєРІРµСЃС‚ РЅР° РІСЃС‚СѓРїР»РµРЅРёРµ РІ РљРѕСЂРѕР»РµРІСЃС‚РІРѕ РЁС‚РѕСЂРјРіСЂР°Рґ
 					f = law_faction
 				end
 				if f and player:GetPhaseMask() == 1 then
-				--	Игрок выполнил один из квестов и находится в 1 фазе
+				--	РРіСЂРѕРє РІС‹РїРѕР»РЅРёР» РѕРґРёРЅ РёР· РєРІРµСЃС‚РѕРІ Рё РЅР°С…РѕРґРёС‚СЃСЏ РІ 1 С„Р°Р·Рµ
 					local zone, trueZone, r = player:GetZoneId(), false, 0
 					if zone == 1519 then
-					--	Игрок в Штормграде
+					--	РРіСЂРѕРє РІ РЁС‚РѕСЂРјРіСЂР°РґРµ
 						r = 3
 						trueZone = true
 					elseif zone == 10237 or zone == 10214 or zone == 10197 or zone == 10160 or zone == 10179 or zone == 10232 or player:GetNearestCreature( 25, entry_npc ) then
-					--	Игрок играет на полигоне
+					--	РРіСЂРѕРє РёРіСЂР°РµС‚ РЅР° РїРѕР»РёРіРѕРЅРµ
 						r = 2
 						trueZone = true
 					end
 					if trueZone and ActionTime() then
-					--	Если время суперактива - идёт маленький бонус.
+					--	Р•СЃР»Рё РІСЂРµРјСЏ СЃСѓРїРµСЂР°РєС‚РёРІР° - РёРґС‘С‚ РјР°Р»РµРЅСЊРєРёР№ Р±РѕРЅСѓСЃ.
 						r = r + 2
 					end
 					if trueZone and player:GetNearestCreature( 30, entry_bonus_npc ) then
-					--	Если рядом есть НПС дарующий бонус
+					--	Р•СЃР»Рё СЂСЏРґРѕРј РµСЃС‚СЊ РќРџРЎ РґР°СЂСѓСЋС‰РёР№ Р±РѕРЅСѓСЃ
 						r = r + 2
 					end
-					--	Начисление репутации
+					--	РќР°С‡РёСЃР»РµРЅРёРµ СЂРµРїСѓС‚Р°С†РёРё
 					player:SetReputation( f, player:GetReputation( f ) + r )
 				end
 			end
