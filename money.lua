4e68bd64f89a4e77480758cb0f564cd832325505
AcoStar <greenshiftkaito@hotmail.com>
2020-12-13 18:48:10 +0300

    Разработка завершена, Тестирование

--- money.lua
+++ money.lua
@@ -1,16 +1,55 @@
+
+--	NPC заменяющий зону Штормграда (Если есть в радиусе 25 ярдов)
+local entry_npc = 123
+--	NPC создающий зону с бонусом (Для общесерв ивентов)
+local entry_bonus_npc = 123
+
 --[[Every 15 minutes player recieve 10 copper. If pleyer is in guild, he auto-deposit 10 copper to guild, but resieve 5 additional copper]]
 local function calculateMoney()
 	local onlinePlayers = GetPlayersInWorld( 2 ); --[[ 2-neutral, both horde and aliance]]		
-	for _, player in ipairs(onlinePlayers) do	
-		if (player:IsAFK() == false) then
+	for _, player in ipairs( onlinePlayers ) do	
+		if ( player:IsAFK() == false ) then
+		--	Добавление денег
 			player:ModifyMoney( 40 );
 			local guild = player:GetGuild();
-			if (guild ~= nil) then		
+			if ( guild ~= nil ) then		
 				player:ModifyMoney( -5 );
 				guild:DepositBankMoney( player, 15 )
-			end			
+			end
+		--	Добавление репутации
+			if SocialTime() then
+				local f
+				if player:GetQuestStatus( 110052 ) == 6 then
+				--	Игрок выполнил квест на вступление в Тени Штормграда
+					f = thiefs_faction
+				elseif player:GetQuestStatus( 110053 ) == 6 then
+				--	Игрок выполнил квест на вступление в Королевство Штормград
+					f = law_faction
+				end
+				if f and player:GetPhaseMask() == 1 then
+				--	Игрок выполнил один из квестов и находится в 1 фазе
+					local zone, r = player:GetZoneId(), 0
+					if zone == 1519 then
+					--	Игрок в Штормграде
+						r = 3
+					elseif zone == 10237 or zone == 10214 or zone == 10197 or zone == 10160 or player:GetNearestCreature( 25, entry_npc ) then
+					--	Игрок играет на полигоне
+						r = 2
+					end
+					if ActionTime() then
+					--	Если время суперактива - идёт маленький бонус.
+						r = r + 2
+					end
+					if player:GetNearestCreature( 30, entry_bonus_npc ) then
+					--	Если рядом есть НПС дарующий бонус
+						r = r + 2
+					end
+					--	Начисление репутации
+					player:SetReputation( f, player:GetReputation( f ) + r )
+				end
+			end
 		end;	  
 	end
 end
-
-CreateLuaEvent(calculateMoney, 900000, 0);
+CreateLuaEvent(calculateMoney, 5000, 0);
+--CreateLuaEvent(calculateMoney, 900000, 0);
