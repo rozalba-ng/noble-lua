326996fce9c3b16e8f30ea4a7bea730b285c3e04
AcoStar <greenshiftkaito@hotmail.com>
2020-12-16 20:06:58 +0300

    Ограничения выбора ролей, смена роли

--- Stormwind system/social_role_select_restrictions.lua
+++ Stormwind system/social_role_select_restrictions.lua
@@ -11,18 +11,58 @@ local entry = {
 
 --[[	ОГРАНИЧЕНИЕ ВЫБОРА ФРАКЦИИ	]]--
 --	Игрок может выбрать фракцию Тени Штормграда только имея социальную роль Вольного Жителя.
+--	Игрок также не может выбрать фракцию до выбора социальной роли.
 
-local function Creature_Gossip( event, player, creature, sender, intid )
+-- Агитатор Теней
+local function Creature_Gossip( event, player, creature )
 	local text
-	if player:HasAura(91058) then
-	--	Игрок вольный житель
-		text = "Заплутал? Могу подсказать дорогу."
-		player:GossipAddQuests( creature )
+	local Q = CharDBQuery( "SELECT id FROM character_citycraft_config WHERE character_guid = "..player:GetGUIDLow() )
+	if Q then
+	--	Игрок уже выбрал социальную роль
+		if player:HasAura(91058) then
+		--	Игрок вольный житель
+			text = "Заплутал? Могу подсказать дорогу."
+			player:GossipAddQuests( creature )
+		else
+		--	Игрок имеет другую социальную роль
+			text = "Я с такими как ты не вожусь, понятно?\n\n|cffa60702Обратите внимание, для вступления в Тени Штормграда вы должны быть Вольным Жителем."
+		end
 	else
-	--	Игрок имеет другую социальную роль
-		text = "У меня для тебя дел нет, проваливай."
+	--	Игрок ещё не выбрал социальную роль
+		text = "Я тебя не знаю. Когда покажешь себя - приходи.\n\n|cffa60702Вы ещё не выбрали социальную роль. Для её выбора поищите Джибби - он должен быть где-то неподалёку.\n\nОбратите внимание, для вступления в Тени Штормграда вы должны быть Вольным Жителем."
 	end
 	player:GossipSetText( text, 16122001 )
 	player:GossipSendMenu( 16122001, creature )
 end
 RegisterCreatureGossipEvent( entry.questgiver.thief, 1, Creature_Gossip ) -- GOSSIP_EVENT_ON_HELLO
+
+--	Агитатор Штормграда
+local function Creature2_Gossip( event, player, creature )
+	local text
+	local Q = CharDBQuery( "SELECT id FROM character_citycraft_config WHERE character_guid = "..player:GetGUIDLow() )
+	if Q then
+	--	Игрок уже выбрал социальную роль
+		text = "Рад видеть тебя."
+		player:GossipAddQuests( creature )
+	else
+	--	Игрок ещё не выбрал социальную роль
+		text = "Ты в городе недавно, да?\n\n|cffa60702Вы ещё не выбрали социальную роль. Для её выбора поищите Джибби - он должен быть где-то неподалёку."
+	end
+	player:GossipSetText( text, 16122003 )
+	player:GossipSendMenu( 16122003, creature )
+end
+RegisterCreatureGossipEvent( entry.questgiver.law, 1, Creature_Gossip ) -- GOSSIP_EVENT_ON_HELLO
+
+--[[	ПРЕДУПРЕЖДЕНИЕ О НЕДОПУСТИМОЙ КОМБИНАЦИИ РОЛИ И ФРАКЦИИ	]]--
+
+local function Player_OnLogin( _, player )
+	local Q = CharDBQuery( "SELECT city_class FROM character_citycraft_config WHERE character_guid = "..player:GetGUIDLow() )
+	if Q then
+		local role = Q:GetUInt32(0)
+		if ( role ~= 91058 ) and player:GetQuestStatus( 110052 ) == 6 then
+		--	Игрок вступил в Тени Штормграда имея не ту роль
+			player:SendBroadcastMessage("|cffFF4500[!!]|r Только игроки имеющие социальную роль \"Вольный Житель\" могут находиться в Тенях Штормграда.")
+		end
+	end
+end
+RegisterPlayerEvent( 3, Player_OnLogin ) -- PLAYER_EVENT_ON_LOGIN
--- Stormwind system/social_role_select_stormwind_SERVER.lua
+++ Stormwind system/social_role_select_stormwind_SERVER.lua
@@ -12,6 +12,9 @@ local aura = {
 	91062, -- Аноним
 }
 
+local entry_quest_law = 110053
+local entry_quest_thief = 110052
+
 function MyHandlers.ShowMenu( player )
 	AIO.Handle(player, "SocialClassSelection","Storm_ShowMenu")
 end
@@ -21,10 +24,16 @@ function MyHandlers.SelectClass( player, class )
 	if creature and tonumber(class) then
 		class = math.floor( tonumber(class) )
 		if class > 0 and class < 5 then
-			CharDBQuery("REPLACE INTO character_citycraft_config ( character_guid, city_class ) values ("..player:GetGUIDLow()..", "..aura[class]..")")
-			player:AddAura( aura[class], player )
-			player:CompleteQuest( entry_quest )
-			player:TalkingHead( creature, "А ты умеешь выбирать жизненные пути, да? Рад знакомству." )
+			if player:HasQuest( entry_quest ) then
+				CharDBQuery("REPLACE INTO character_citycraft_config ( character_guid, city_class ) values ("..player:GetGUIDLow()..", "..aura[class]..")")
+				player:AddAura( aura[class], player )
+				player:CompleteQuest( entry_quest )
+				player:RewardQuest( entry_quest )
+				player:TalkingHead( creature, "А ты умеешь выбирать жизненные пути, да? Рад знакомству." )
+			elseif player:GetData("ChangingSocialRole") then
+				player:SetData( "ChangingSocialRole_Selected", aura[class] )
+				Creature_Gossip( 3, player, creature )
+			end
 		end
 	end
 end
@@ -56,12 +65,92 @@ local function Creature_Gossip( event, player, creature, sender, intid )
 				player:GossipMenuAddItem( 0, "<Представиться.>", 1, 1 )
 			end
 		end
+		local Q = CharDBQuery( "SELECT allow_role_change FROM character_citycraft_config WHERE character_guid = "..player:GetGUIDLow() )
+		if Q then
+		--	Если игрок уже смешарик
+			if Q:GetUInt8(0) > 0 then
+				player:GossipMenuAddItem( 0, "<Сменить социальную роль.>" 1, 2, false, "ВСЯ РЕПУТАЦИЯ БУДЕТ ПОТЕРЯНА.\nВы не сможете бесплатно сменить социальную роль ещё раз." )
+			end
+		end
 		player:GossipSetText( text, 13122001 )
 		player:GossipSendMenu( 13122001, creature )
-	elseif player:HasQuest( entry_quest ) then
+	elseif event == 3 then
+	--	Вывод госсипа при смене социальной роли
+		local text = "Обратите внимание - фракция Теней Штормграда доступна только для Вольных Жителей.\n\n|cffa60702Вся накопленная репутация будет потеряна.\n\nВы не сможете бесплатно сменить социальную роль ещё раз.\n\n|rВыберите фракцию:"
+		player:GossipMenuAddItem( 0, "Королевство Штормград", 2, 1, false, "Это ваш окончательный выбор." )
+		if player:GetData("ChangingSocialRole_Selected") == 91058 then
+		--	Игрок выбрал Вольного Жителя
+			player:GossipMenuAddItem( 0, "Тени Штормграда", 2, 2, false, "Это ваш окончательный выбор." )
+		end
+		player:GossipMenuAddItem( 0, "Я хочу выбрать другую социальную роль.", 2, 3 )
+		player:GossipSetText( text, 16122002 )
+		player:GossipSendMenu( 16122002, creature )
+	else
 	--	Выбор варианта
-		player:GossipComplete()
-		MyHandlers.ShowMenu( player )
+		if sender == 1 then
+		--	Выбор роли
+			if intid == 2 then
+				player:SetData( "ChangingSocialRole", true )
+			end
+			player:GossipComplete()
+			MyHandlers.ShowMenu( player )
+		else
+		--	Перевыбор роли
+			if player:GetData("ChangingSocialRole") and intid ~= 3 then
+			--	Последняя проверка, определение оставшихся смен роли (Если игрок имел > 1)
+				local Q = CharDBQuery( "SELECT allow_role_change FROM character_citycraft_config WHERE character_guid = "..player:GetGUIDLow() )
+				if Q then
+					local allow_role_change = Q:GetUInt8(0)
+					if allow_role_change > 0 then
+						allow_role_change = allow_role_change - 1
+					--	Отбирание репутационных жетонов
+					--[[
+						if player:HasItem( 2114371, 1, true ) then
+							player:RemoveItem( 2114371, player:GetItemCount( 2114371, true ) )
+						end						
+						if player:HasItem( 2114372, 1, true ) then
+							player:RemoveItem( 2114372, player:GetItemCount( 2114372, true ) )
+						end
+					]]
+					--	Настройка выполненных квестов
+						if intid == 1 then
+						--	Королевство Штормград
+							player:RemoveQuest( entry_quest_thief )
+							player:RemoveQuest( entry_quest_law )
+							player:AddQuest( entry_quest_law )
+							player:CompleteQuest( entry_quest_law )
+							player:RewardQuest( entry_quest_law )
+						elseif ( ( intid == 2 ) and ( player:GetData("ChangingSocialRole_Selected") == 91058 ) ) then
+						--	Тени Штормграда
+							player:RemoveQuest( entry_quest_thief )
+							player:RemoveQuest( entry_quest_law )
+							player:AddQuest( entry_quest_thief )
+							player:CompleteQuest( entry_quest_thief )
+							player:RewardQuest( entry_quest_thief )
+						end
+					--	Установка нулевой репутации
+						player:SetReputation( thiefs_faction, 0 )
+						player:SetReputation( law_faction, 0 )
+					--	Махинации с аурами
+						for i = 1, #aura do
+							player:RemoveAura( aura[i] )
+						end
+						player:AddAura( player:GetData("ChangingSocialRole_Selected") )
+					--	Обновление записи в базе данных
+						CharDBQuery("REPLACE INTO character_citycraft_config ( character_guid, city_class, allow_role_change ) values ("..player:GetGUIDLow()..", "..tonumber(player:GetData("ChangingSocialRole_Selected"))..", "..allow_role_change..")")
+					--	Подчищаем кеш, выводим уведомление.
+						player:SetData( "ChangingSocialRole", nil )
+						player:SetData( "ChangingSocialRole_Selected", nil )
+						player:TalkingHead( creature, "Все имеют право на второй шанс." )
+						player:SendBroadcastMessage( "Вы изменили свою социальную роль. Осталось смен роли на персонаже: "..allow_role_change )
+					end
+				end
+			else
+			--	Выбор другой социальной роли.
+				player:GossipComplete()
+				MyHandlers.ShowMenu( player )
+			end
+		end
 	end
 end
 RegisterCreatureGossipEvent( entry_creature, 1, Creature_Gossip ) -- GOSSIP_EVENT_ON_HELLO
