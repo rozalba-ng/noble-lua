f93b2e60a5e74baf62decf4155ccbd888f6676be
AcoStar <greenshiftkaito@hotmail.com>
2020-12-17 05:10:47 +0300

    Сбивание сосулек

--- Stormwind system/Quests/Winter/FreeResidents.lua
+++ Stormwind system/Quests/Winter/FreeResidents.lua
@@ -13,6 +13,23 @@ local quests = {
 --	"Сбить сосульки"
 
 local function OnClick_Icicle( event, go, player )
+	local name = player:GetName()
+	if not quests[1].players[name] then
+		quests[1].players[name] = 0
+	end
+	quests[1].players[name] = quests[1].players[name] + 1
+	if quests[1].players[name] >= 10 then
+		player:CompleteQuest( quests[1].entry )
+		player:SendAreaTriggerMessage("Сосульки сбиты.")
+	--	Обновление фазы для корректного отображения иконок квестов
+		player:SetPhaseMask(524288)
+		player:SetPhaseMask(1)
+	else
+		player:SendAreaTriggerMessage("Сбито "..quests[1].players[name].." сосулек из 10.")
+	end
+	local x,y,z = go:GetLocation()
+	local creature = go:SpawnCreature( quests[1].npc, x, y, z, 0, 3, 2000 )
+	player:CastSpell( creature, quests[1].spell, true )
 	go:Despawn()
 end
 RegisterGameObjectEvent( quests[1].gameobject, 14, OnClick_Icicle ) -- GAMEOBJECT_EVENT_ON_USE
