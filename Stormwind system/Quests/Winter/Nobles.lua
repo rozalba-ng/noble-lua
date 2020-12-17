bbb9d901a2d4b94538281f8270d51ad65dacbfaf
AcoStar <greenshiftkaito@hotmail.com>
2020-12-17 03:00:19 +0300

    Bug fix

--- Stormwind system/Quests/Winter/Nobles.lua
+++ Stormwind system/Quests/Winter/Nobles.lua
@@ -11,20 +11,16 @@ local quests = {
 --	"Помощь бедным"
 
 local function Gossip_HungryBeggar( event, player, creature, sender, intid )
-	local guid = player:GetName()
+	local name = player:GetName()
 	if event == 1 then
 		local text
-		if not quests[1].players[guid] then
-			quests[1].players[guid] = 0
-		--	
-		--	ПРОБЛЕМА
-		--	Тут кладём в таблицу число
-			print(quests[1].players[guid]) -- Тут ноль
+		if not quests[1].players[name] then
+			quests[1].players[name] = 0
 		end
-		if creature:GetData("Fed") and ( ( os.time() - creature:GetData("Fed") ) > 300 ) then
+		if creature:GetData("Fed") and ( ( os.time() - creature:GetData("Fed") ) <= 300 ) then
 		--	Нищий недавно накормлен
 			text = Roulette( "А?", "Ась?", "Не мешай.", "<Нищий сыто икает.>" )
-		elseif player:HasQuest( quests[1].entry ) and not ( quests[1].players[guid] >= 5 ) then
+		elseif player:HasQuest( quests[1].entry ) and not ( quests[1].players[name] >= 5 ) then
 		--	Игрок имеет задание
 			text = Roulette( "Есть поесть?", "Подайте монетку!", "Живот сводит...", "Знали бы вы, как я голоден.", "Что там у вас?" )
 			player:GossipMenuAddItem( 0, "<Накормить голодающего.>", 1, 1 )
@@ -42,16 +38,12 @@ local function Gossip_HungryBeggar( event, player, creature, sender, intid )
 	else
 		creature:SetData( "Fed", os.time() )
 		player:TalkingHead( creature, Roulette( "Вот это по нашему!", "Спасибо, добрейший человек.", "Сейчас перекусим.", "Благодарю! Всех вам благ!", "Ух... Горяченькое!", "Солички не найдётся?", "Сытым дольго не пробудешь..." ) )
-		--
-		--	ПРОБЛЕМА
-		--	Тут его уже нет. Вопрос: Где?
-		print(quests[1].players[guid]) -- Тут уже nil
-		quests[1].players[guid] = quests[1].players[guid] + 1
-		if quests[1].players[guid] >= 5 then
+		quests[1].players[name] = quests[1].players[name] + 1
+		if quests[1].players[name] >= 5 then
 			player:CompleteQuest( quests[1].entry )
 			player:SendAreaTriggerMessage("Горячие блюда розданы.")
 		else
-			player:SendAreaTriggerMessage("Накормлено "..quests[1].players[guid].." нищих из 5.")
+			player:SendAreaTriggerMessage("Накормлено "..quests[1].players[name].." нищих из 5.")
 		end
 	end
 end
