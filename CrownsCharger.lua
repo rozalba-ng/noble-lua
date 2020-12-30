4b7f76d7f1d5f796b6ec1992d7b61266abfa3519
Саша <rozalba89@gmail.com>
2020-12-30 18:53:04 +0300

    change veksels

---
+++ CrownsCharger.lua
@@ -0,0 +1,67 @@
+local NpcChargerId = 110004;
+local veksel = 600157;
+local crown = 600057;
+local faction_stormwind = 1162;
+local faction_shadow_stormwind = 1163;
+local reputation_friendly = 3000;
+local reputation_honored = 9000;
+local reputation_revered = 21000;
+local MenuId = 13213;
+
+local function OnGossipCharger(event, player, object)
+    player:GossipClearMenu() -- required for player gossip
+    player:GossipMenuAddItem(0, "Купить вексели (в поле 'код' вводите желаемое количество)", 1, 2, true, nil)
+    player:GossipMenuAddItem(0, "Продать вексели (в поле 'код' вводите желаемое количество)", 1, 3, true, nil)
+    player:GossipMenuAddItem(0, "Закрыть", 1, 5)
+    player:GossipSetText( 'Приветствую! <br>ООС: Тут вы можете обменять ваши персональные короны на не-персональные вексели и обратно. Векселями можно обмениваться с другими игроками. <br>Курс покупки: <br>- дружелюбие - 5 корон за вексель<br>- уважение - 4 короны за вексель<br>- почтение - 3 короны за вексель', MenuId )
+    player:GossipSendMenu(1, object, MenuId) -- MenuId required for player gossip
+end
+
+local function OnGossipChargerSelect(event, player, object, sender, intid, code, menuid)
+    if (intid == 2) then
+        local num = tonumber(code);
+        local crownsNeeded;
+
+        if ((player:GetReputation( faction_stormwind ) >= reputation_revered) or (player:GetReputation( faction_shadow_stormwind ) >= reputation_revered)) then
+            crownsNeeded = num*3;
+        elseif ((player:GetReputation( faction_stormwind ) >= reputation_honored) or (player:GetReputation( faction_shadow_stormwind ) >= reputation_honored)) then
+            crownsNeeded = num*4;
+        elseif ((player:GetReputation( faction_stormwind ) >= reputation_friendly) or (player:GetReputation( faction_shadow_stormwind ) >= reputation_friendly)) then
+            crownsNeeded = num*5;
+        else
+            player:SendBroadcastMessage("У вас недостаточно репутации для покупки векселей. Минимальная репутация: дружелюбие в одной из фракций Штормграда");
+            return false;
+        end
+
+        if (player:HasItem(crown, crownsNeeded)) then
+            local item = player:AddItem(veksel, num);
+            if(item == nil)then
+                player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет места.|r");
+                return false;
+            end
+            player:RemoveItem(crown, crownsNeeded);
+        else
+            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНедостаточно корон для покупки нужного числа векселей. Требуемое число корон: |r" .. tostring(crownsNeeded));
+        end
+        OnGossipCharger(event, player, object)
+    elseif (intid == 3) then
+        local num = tonumber(code);
+        local crownsToAdd = num * 3;
+        if (player:HasItem(veksel, num)) then
+            local item = player:AddItem(crown, crownsToAdd);
+            if(item == nil)then
+                player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет места.|r");
+                return false;
+            end
+            player:RemoveItem(veksel, num);
+        else
+            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНедостаточно векселей. Требуемое число векселей: |r" .. tostring(num));
+        end
+        OnGossipCharger(event, player, object)
+    elseif (intid == 5) then
+        player:GossipComplete()
+    end
+end
+
+RegisterCreatureGossipEvent(NpcChargerId, 1, OnGossipCharger)
+RegisterCreatureGossipEvent(NpcChargerId, 2, OnGossipChargerSelect)
