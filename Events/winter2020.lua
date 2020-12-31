7b95aa98b6a8b8d0c17c7cf15bc199dd5151d2ac
AcoStar <greenshiftkaito@hotmail.com>
2020-12-31 10:25:10 +0300

    Добавлены новые стадии квеста

--- Events/winter2020.lua
+++ Events/winter2020.lua
@@ -126,38 +126,6 @@ for i = 1, #creatures do
 	RegisterCreatureGossipEvent( creatures[i], 2, Gossip_WinterPet ) -- GOSSIP_EVENT_ON_SELECT
 end
 
---[[	ВХОД ИГРОКА В МИР	]]--
-
-local items = {
-	[1] = {
-		"Дорогой друг! Я с радостью слежу за твоими успехами и желаю тебе дальнейших процветаний! Пускай эти подарки принесут тебе праздничное настроение.\n\n  Дедушка Зима.",
-		"Дорогой друг! Я вижу, кое-кто в этом году вел себя не самым лучшим образом, чем сильно огорчил мое старческое сердце. По правде сказать, сначала я хотел положить в твой подарок большой кусок угля, но в последний момент передумал. Этот год был непростым, так что каждый заслужил немного новогоднего настроения. Счастливого зимнего покрова!",
-		"Дорогой друг! Я рад, что ты стойко движешься к своим целям и не сворачиваешь с пути! Пускай эти подарки принесут тебе праздничное настроение.\n\n  Дедушка Зима.",
-	},
-	[2] = {
-		1000100,
-		1000101,
-		1000102,
-	},
-	[3] = {
-		5057393,
-		5057394,
-		5057392,
-	},
-}
-
-local function OnLogin_Player( _, player )
-	local Q = WorldDBQuery( "SELECT behavior, companion, item FROM Winter2020 WHERE character_guid = "..player:GetGUIDLow().." AND issued = 0" )
-	if Q then
-		local text = Q:GetUInt8(0)
-		local companion = Q:GetUInt8(1)
-		local item = Q:GetUInt8(2)
-		SendMail( "Посылка от Дедушки Зимы.", items[1][text], player:GetGUIDLow(), 0, 65, 20, 0, 0, items[2][companion], 1, items[3][item], 1, 600158, 1 )
-		WorldDBQuery( "UPDATE Winter2020 SET issued = 1 WHERE account = "..player:GetAccountId() )
-	end
-end
-RegisterPlayerEvent( 3, OnLogin_Player ) -- PLAYER_EVENT_ON_LOGIN
-
 --[[	СУПЕРСЕКРЕТНЫЙ КВЕСТ	]]--
 
 local entry_ice = 5049499
@@ -165,6 +133,8 @@ local entry_trampoline = 5049500
 local entry_cacao = 5049501
 local entry_gnome = 9929896
 local entry_hammer = 5049502
+local entry_gift = 5049503
+local entry_acostar = 9921754
 --	|cff80d2ff
 
 local function GetQuestStage( player )
@@ -204,7 +174,7 @@ local function Stage1( _, _, player )
 		UPQuestStage(player) --> 6
 	elseif stage == 6 then
 	--	Игрок дубасит сосульку 4.
-		player:SendBroadcastMessage("|cff80d2ffКажется звон исходит из стоящего рядом портала. В вашей голове возникают два слова: \n\"Старый мир\".")
+		player:SendBroadcastMessage("|cff80d2ffКажется звон исходит из стоящего рядом портала. В вашей голове возникают два слова: \n\"|cff8880ffСтарый мир|r\".")
 		UPQuestStage(player) --> 7
 	else
 	--	Игрок тут чисто по приколу.
@@ -221,7 +191,7 @@ local function Stage2( _, _, player )
 		UPQuestStage(player) --> 2
 	elseif stage == 2 then
 	--	Игрок уже с молотком.
-		player:SendBroadcastMessage("|cff80d2ffВы уже взяли молоток. Теперь надо найти то что можно хорошо стукнуть.")
+		player:SendBroadcastMessage("|cff80d2ffВы уже взяли молоток. Теперь надо найти то, что можно хорошо стукнуть.")
 	else
 	--	Игрок тут чисто по приколу.
 		player:SendBroadcastMessage("|cff80d2ff\"Воровать чужие молотки - не самая лучшая идея.\"")
@@ -236,7 +206,7 @@ local function Stage3( _, object, player )
 	y = y + math.random(-1,1)
 	z = z + 1.4
 	player:MoveJump( x, y, z, 0.2, 40 )
-	local creature = player:GetNearestCreature( 40, entry_gnome )
+	local creature = player:GetNearestCreature( 60, entry_gnome )
 	if creature then
 		local stage = GetQuestStage(player)
 		if stage == 7 then
@@ -265,3 +235,116 @@ local function Stage3( _, object, player )
 	end
 end
 RegisterGameObjectEvent( entry_trampoline, 14, Stage3 ) -- GAMEOBJECT_EVENT_ON_USE
+
+local function Stage4( _, _, player )
+	local stage = GetQuestStage(player)
+	if stage == 13 then
+	--	Игрок только пришёл.
+		player:SendBroadcastMessage("|cff80d2ffВокруг мрачно, будто вы снова попали в Гилнеас.")
+		UPQuestStage(player) --> 14
+	elseif stage == 14 then
+	--	Игрок снова нажимает на какао.
+		player:SendBroadcastMessage("|cff80d2ff\"Это какао - единственное, что походит на подарок к Зимнему Покрову. Я возьму его.\"")
+		UPQuestStage(player) --> 15
+	else
+	--	Игрок тут чисто по приколу.
+		player:SendBroadcastMessage("|cff80d2ff...")
+	end
+end
+RegisterGameObjectEvent( entry_cacao, 14, Stage4 ) -- GAMEOBJECT_EVENT_ON_USE
+
+local function Stage6( _,_,_, player )
+	if GetQuestStage(player) == 17 then
+		player:SendBroadcastMessage("|cff80d2ff\"Ну и что делать дальше? А главное - кто украл эти подарки и зачем разбросал их по миру?\"\n|cff80d2ff\"А может задание не работает? Мне нужно найти Акостара на лунной поляне.\"")
+		UPQuestStage(player) --> 18
+	end
+end
+
+local function Stage5( _, _, player )
+	local stage = GetQuestStage(player)
+	if stage == 15 then
+	--	Игрок только пришёл.
+		player:SendBroadcastMessage("|cff80d2ff\"Подарок тикает, а значит внутри бомба или часы, но наверное всё же бомба..\"")
+		UPQuestStage(player) --> 16
+	elseif stage == 16 then
+	--	Игрок снова нажимает на какао.
+		player:SendBroadcastMessage("|cff80d2ff\Вы забираете подарок.")
+		UPQuestStage(player) --> 17
+		player:RegisterEvent( Stage6, 300000, 1 )
+	else
+	--	Игрок тут чисто по приколу.
+		player:SendBroadcastMessage("|cff80d2ffПодарок тикает.")
+	end
+end
+RegisterGameObjectEvent( entry_gift, 14, Stage5 ) -- GAMEOBJECT_EVENT_ON_USE
+
+local taxi = { { 1, 7872.4, -2482.8, 488.1 }, { 1, 7853.8, -2470.5, 492.3 }, { 1, 7827.3, -2460.0, 496.4 }, { 1, 7723.8, -2488.6, 499.9 }, { 1, 7638.7, -2668.7, 543.2 }, { 1, 7556.0, -2889.8, 574.0 }, { 1, 7574.4, -2986.3, 600.2 }, { 1, 7695.3, -3060.7, 631.8 }, { 1, 7746.3, -3087.8, 636.9 }, { 1, 7780.2, -3147.5, 651.6 }, { 1, 7789.8, -3186.5, 632.0 }, { 1, 7789.8, -3190.5, 625.7 }, }
+taxi = AddTaxiPath( taxi, 36966, 36966 )
+
+local function Stage7( event, player, creature, sender, intid, code )
+	if event == 1 then
+		local text = "Что я здесь делаю? Прячусь, конечно!"
+		if ( GetQuestStage(player) >= 18 and GetQuestStage(player) <= 20 ) then
+			text = text.."\nА вам что нужно? Тоже задание проходите? Гипер сложное, говорите? Что уж тут теперь поделать... Но я могу перенести вас на следующий этап, конечно. Если у вас есть СЕКРЕТНЫЙ КОД. И не вздумайте спрашивать его у меня в дискорде! Нет, я серьёзно. Не надо.."
+			player:GossipMenuAddItem( 0, "<Назвать код на ушко.>", 1, 1, true )
+		else
+			text = text.."\nНо раз уж меня нашли - поздравляю с Новым годом. Всего вам хорошего. Чтобы здоровье не шалило, деньги были в карманах и мордашка не унывала."
+		end
+		player:GossipSetText( text, 31122001 )
+		player:GossipSendMenu( 31122001, creature )
+	else
+		if code then
+			if code == "7819" then
+				if GetQuestStage(player) == 18 then
+					player:TalkingHead( creature, "Добро пожаловать в следующий этап!" )
+					UPQuestStage(player) --> 19
+				end
+				player:StartTaxi(taxi)
+			else
+				creature:AddAura( 21847, player )
+				creature:SendChatMessageToPlayer( 12, 0, Roulette( "Не-а.", "Не то!", "Безуспешная попытка.", "Не верно.", "Нет.", "Попробуй ещё раз.", "Ух... Не то.", ), player )
+				player:GossipComplete()
+			end
+		else
+			player:GossipComplete()
+		end
+	end
+end
+RegisterCreatureGossipEvent( entry_acostar, 1, Stage7 ) -- GOSSIP_EVENT_ON_HELLO
+RegisterCreatureGossipEvent( entry_acostar, 2, Stage7 ) -- GOSSIP_EVENT_ON_SELECT
+
+--[[	ВХОД ИГРОКА В МИР	]]--
+
+local items = {
+	[1] = {
+		"Дорогой друг! Я с радостью слежу за твоими успехами и желаю тебе дальнейших процветаний! Пускай эти подарки принесут тебе праздничное настроение.\n\n  Дедушка Зима.",
+		"Дорогой друг! Я вижу, кое-кто в этом году вел себя не самым лучшим образом, чем сильно огорчил мое старческое сердце. По правде сказать, сначала я хотел положить в твой подарок большой кусок угля, но в последний момент передумал. Этот год был непростым, так что каждый заслужил немного новогоднего настроения. Счастливого зимнего покрова!",
+		"Дорогой друг! Я рад, что ты стойко движешься к своим целям и не сворачиваешь с пути! Пускай эти подарки принесут тебе праздничное настроение.\n\n  Дедушка Зима.",
+	},
+	[2] = {
+		1000100,
+		1000101,
+		1000102,
+	},
+	[3] = {
+		5057393,
+		5057394,
+		5057392,
+	},
+}
+
+local function OnLogin_Player( _, player )
+	local Q = WorldDBQuery( "SELECT behavior, companion, item FROM Winter2020 WHERE character_guid = "..player:GetGUIDLow().." AND issued = 0" )
+	if Q then
+		local text = Q:GetUInt8(0)
+		local companion = Q:GetUInt8(1)
+		local item = Q:GetUInt8(2)
+		SendMail( "Посылка от Дедушки Зимы.", items[1][text], player:GetGUIDLow(), 0, 65, 20, 0, 0, items[2][companion], 1, items[3][item], 1, 600158, 1 )
+		WorldDBQuery( "UPDATE Winter2020 SET issued = 1 WHERE account = "..player:GetAccountId() )
+		-- --
+		if GetQuestStage(player) == 17 then
+			player:RegisterEvent( Stage6, 600000, 1 )
+		end
+	end
+end
+RegisterPlayerEvent( 3, OnLogin_Player ) -- PLAYER_EVENT_ON_LOGIN
