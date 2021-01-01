180db0d7cd9750b73702387174bd662d2e77a5aa
Саша <rozalba89@gmail.com>
2021-01-02 01:13:02 +0300

    rep auras

--- money.lua
+++ money.lua
@@ -19,47 +19,47 @@ local function calculateMoney()
 				guild:DepositBankMoney( player, 10 )
 			end
 		--	Добавление репутации
---			if SocialTime() then
---				local f
---				if player:GetQuestStatus( 110052 ) == 6 then
---				--	Игрок выполнил квест на вступление в Тени Штормграда
---					f = thiefs_faction
---				elseif player:GetQuestStatus( 110053 ) == 6 then
---				--	Игрок выполнил квест на вступление в Королевство Штормград
---					f = law_faction
---				end
---				if f and player:GetPhaseMask() == 1 then
---				--	Игрок выполнил один из квестов и находится в 1 фазе
---					local zone, trueZone, r = player:GetZoneId(), false, 0
---					if ( zone == 1519 or player:HasAura(guildzone_aura) ) then
---					--	Игрок в Штормграде
---						r = 4
---						trueZone = true
---					elseif ( zone == 10237 or zone == 10214 or zone == 10197 or zone == 10160 or zone == 10179 or zone == 10232 ) then
---					--	Игрок играет на полигоне
---						r = 3
---						trueZone = true
+			if SocialTime() then
+				local f
+				if player:GetQuestStatus( 110052 ) == 6 then
+				--	Игрок выполнил квест на вступление в Тени Штормграда
+					f = thiefs_faction
+				elseif player:GetQuestStatus( 110053 ) == 6 then
+				--	Игрок выполнил квест на вступление в Королевство Штормград
+					f = law_faction
+				end
+				if f and player:GetPhaseMask() == 1 then
+				--	Игрок выполнил один из квестов и находится в 1 фазе
+					local zone, trueZone, r = player:GetZoneId(), false, 0
+					if ( zone == 1519 or player:HasAura(guildzone_aura) ) then
+					--	Игрок в Штормграде
+						r = 4
+						trueZone = true
+					elseif ( zone == 10237 or zone == 10214 or zone == 10197 or zone == 10160 or zone == 10179 or zone == 10232 ) then
+					--	Игрок играет на полигоне
+						r = 3
+						trueZone = true
+					end
+					if trueZone and ActionTime() then
+					--	Если время суперактива - идёт маленький бонус.
+						r = r + 3
+					end
+--					if trueZone and player:HasAura(CREATIVE_PHASE_AURA) then
+--					--	Если рядом есть НПС дарующий бонус
+--						r = r + 2
 --					end
---					if trueZone and ActionTime() then
---					--	Если время суперактива - идёт маленький бонус.
---						r = r + 3
+					--	Начисление репутации
+					player:SetReputation( f, player:GetReputation( f ) + r )
+					--	Снятие репутации UPD ROZALBA: отменяем снятие репутации
+--					if f == thiefs_faction then
+--					--	Снятие репутации у законников
+--						player:SetReputation( law_faction, player:GetReputation( law_faction ) - r )
+--					else
+--					--	Снятие репутации у плохишей
+--						player:SetReputation( thiefs_faction, player:GetReputation( thiefs_faction ) - r )
 --					end
-----					if trueZone and player:HasAura(CREATIVE_PHASE_AURA) then
-----					--	Если рядом есть НПС дарующий бонус
-----						r = r + 2
-----					end
---					--	Начисление репутации
---					player:SetReputation( f, player:GetReputation( f ) + r )
---					--	Снятие репутации UPD ROZALBA: отменяем снятие репутации
-----					if f == thiefs_faction then
-----					--	Снятие репутации у законников
-----						player:SetReputation( law_faction, player:GetReputation( law_faction ) - r )
-----					else
-----					--	Снятие репутации у плохишей
-----						player:SetReputation( thiefs_faction, player:GetReputation( thiefs_faction ) - r )
-----					end
---				end
---			end
+				end
+			end
 		end;	  
 	end
 end
