5c6efdc194c987048b9990f005658692fcfeca47
AcoStar <greenshiftkaito@hotmail.com>
2021-02-08 14:49:27 +0300

    Совмещение с аддоном на анимации

--- PetControl/PetControl_Client.lua
+++ PetControl/PetControl_Client.lua
@@ -6,6 +6,8 @@ end
 
 local PetControlAIO = AIO.AddHandlers("PetControl", {})
 
+PetControl = PetControl or {}
+
 function PlayerIsCompanionOwner()
 	AIO.Handle( "PetControl", "PlayerIsCompanionOwner" )
 end
@@ -14,12 +16,12 @@ function PetControlAIO.ShowPetControlButton( _, allowed )
 	if allowed then
 		PetControlButton:Show()
 		PetControlButton:Enable()
+		PetControl.Target = true
 	else
 		PetControlButton:Disable()
 	end
 end
 
-PetControl = PetControl or {}
 
 function PetControl.Byte1( byte1 )
 	AIO.Handle( "PetControl", "Byte1", byte1 )
