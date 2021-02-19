fb9c44e6907314be2e2b96143850761e41c7daa9
Саша <rozalba89@gmail.com>
2021-02-19 18:52:13 +0300

    npcsetstatradius

--- BattleSystem/role_stat_system.lua
+++ BattleSystem/role_stat_system.lua
@@ -143,7 +143,7 @@ local function getFormattedRollMessage(attack_type, roller_name, action_type, ta
     if def_rand > 0 then
         return string.format("%s действие %s %s %s |c%s%s|r. \n(%u+%u |c%s%s|r %u+%u)", attack_type, roller_name, action_type, target_name, result_color, result_text, player_att, att_rand, result_color, result_symbol, target_def, def_rand);
     else
-        return string.format("%s действие %s %s %s |c%s%s|r. \n(%u+%u |c%s%s|r %u)", attack_type, roller_name, action_type, target_name, result_color, result_text, player_att, att_rand, result_color, result_symbol, target_def);
+        return string.format("%s действие %s %s %s |c%s%s|r. \n(%u+%u |c%s%s|r порог %u)", attack_type, roller_name, action_type, target_name, result_color, result_text, player_att, att_rand, result_color, result_symbol, target_def);
     end
 end
 
