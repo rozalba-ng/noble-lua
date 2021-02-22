1f83f966f354d4abc69fab998b6fd847b1c9889c
Саша <rozalba89@gmail.com>
2021-02-22 22:51:45 +0300

    убираю бонус за баф

--- BattleSystem/CreatureStats.lua
+++ BattleSystem/CreatureStats.lua
@@ -85,8 +85,10 @@ function setNpcStats(GM_target, stat, value)
     local guid = GM_target:GetDBTableGUIDLow();
     local guidLow = GM_target:GetGUIDLow();
     value = tonumber(value);
-
+print(stat)
+print(value)
     if not (statDbNames[stat] and value and value >= 0) then
+        print(statDbNames[stat])
         return false
     end
 
