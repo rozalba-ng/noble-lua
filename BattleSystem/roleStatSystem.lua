f6310ad815f7bb5e79ef485a31f066a76eaf9c02
Саша <rozalba89@gmail.com>
2021-09-16 01:43:47 +0300

    reforges

--- BattleSystem/roleStatSystem.lua
+++ BattleSystem/roleStatSystem.lua
@@ -630,7 +630,7 @@ function attackRoll(roller, target, spellid)
         if (roller:ToPlayer()) then
             local nearPlayers = roller:GetPlayersInRange(140, 0, 0)
             for index, nearPlayer in pairs(nearPlayers) do
-                if (roller:IsInSameRaidWith(nearPlayer) or target:IsInSameRaidWith(nearPlayer)) then
+                if (roller:IsInSameRaidWith(nearPlayer) or (target:ToPlayer() and target:IsInSameRaidWith(nearPlayer))) then
                     if isFogPotionUsed then -- Зелье тумана
                         nearPlayer:SendBroadcastMessage(getFormattedRollMessage(attack_type, roller_name, action_type, target_name, result_color, result_text, player_att, att_rand, result_color, result_symbol, target_def, def_rand));
                         local itemLink = GetItemLink(600055, 8)
@@ -649,7 +649,7 @@ function attackRoll(roller, target, spellid)
 
             local nearPlayers = roller:GetPlayersInRange(40, 0, 0)
             for index, nearPlayer in pairs(nearPlayers) do
-                if (roller:IsInSameRaidWith(nearPlayer) ~= true and target:IsInSameRaidWith(nearPlayer) ~= true) then
+                if (roller:IsInSameRaidWith(nearPlayer) ~= true and target:ToPlayer() and target:IsInSameRaidWith(nearPlayer) ~= true) then
                     if isFogPotionUsed then -- Зелье тумана
                         nearPlayer:SendBroadcastMessage(getFormattedRollMessage(attack_type, roller_name, action_type, target_name, result_color, result_text, player_att, att_rand, result_color, result_symbol, target_def, def_rand));
                         local itemLink = GetItemLink(600055, 8)
