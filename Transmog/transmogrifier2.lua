6eb0991b09e36a9d9e96fda999e3a29b2ef27c8e
Саша <rozalba89@gmail.com>
2021-12-23 02:06:32 +0300

    transmog waist

--- Transmog/transmogrifier2.lua
+++ Transmog/transmogrifier2.lua
@@ -406,13 +406,16 @@ local function SetFakeEntry(item, entry)
         player:UpdateUInt32Value(PLAYER_VISIBLE_ITEM_1_ENTRYID + (item:GetSlot() * ITEM_SLOT_MULTIPLIER), entry)
 
         if item:GetSlot() == EQUIPMENT_SLOT_WAIST then
+            print(entry)
             local auraItem = WorldDBQuery('SELECT spellid_1 FROM item_template where spellid_1 > 0 and entry = ' .. entry );
             if auraItem then
+                print(333333333)
                 local aura = tonumber(auraItem:GetString(0))
+                print(aura)
                 if player:HasAura(aura) then -- удаляем старую ауру трансмога
                     player:RemoveAura(aura);
-                    player:AddAura( iAuraNew, player ) --добавляем ауру
                 end
+                player:AddAura( iAuraNew, player ) --добавляем ауру
             end
         end
         if not entryMap[pGUID] then
