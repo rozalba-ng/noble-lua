71330149bb3351213c085d1dbd6a092a51cced0f
Ezil Computer <izhevago@mail.ru>
2022-06-01 17:34:17 +0300

    npc guid del extended to DM

--- SmallScripts/DeleteByGUID.lua
+++ SmallScripts/DeleteByGUID.lua
@@ -1,10 +1,5 @@
-local NPCToDelete = {}
-local GOToDelete = {}
-
-
-
-function OnCommand(player,guid)
-	if(player:GetGMRank() > 0)then
+local function OnCommand(player,guid)
+	if(player:GetGMRank() > 0 or player:GetDmLevel() > 0)then
 		if tonumber(guid) == nil then
 			player:Print("Гуид НПС должен быть числом")
 			return false
@@ -20,7 +15,18 @@ function OnCommand(player,guid)
 		local entry = q_entry:GetInt32(0)
 		local mapid = q_entry:GetInt32(1)
 		local creature = GetCreature(guid,entry,mapid)
+		if creature == nil then
+			player:Print("Данное существо либо не найдено в базе, либо не прогруженно в игровом мире. Попробуйте подойти к НПС, чтобы тот мог появится.")
+			return false
+		end
+		if player:GetDmLevel() > 0 and player:GetGMRank() == 0 then
+			if(creature:GetOwner() ~= player)then
+				player:Print("Данный НПС вам не принадлежит")
+				return false
+			end
+		end
 		creature:Delete()
+		player:Print("НПС успешно удален")
 	else
 		player:Print("У вас нет доступа к этой команде.")
 	end
