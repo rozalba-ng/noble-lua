3608085b04375a7d1e7153b82d0eca60d6681de9
Саша <rozalba89@gmail.com>
2022-11-08 17:57:21 +0300

    bonuses tavern

--- system_theramore/moneyTavernTheramore.lua
+++ system_theramore/moneyTavernTheramore.lua
@@ -3,6 +3,8 @@ local aura_main_tavern   = 91320
 local aura_granizon = 91323
 local aura_amarosa  = 91322
 local aura_ratusha  = 91321
+local aura_garpia  = 91324
+local aura_pirate = 91325
 
 --[[Every 30 minutes calculate activity ]]
 local function sendTavernBonusesTheramore()
@@ -11,6 +13,8 @@ local function sendTavernBonusesTheramore()
 	local ratusha_bonus = 0;
 	local garnizon_bonus = 0;
 	local amarosa_bonus = 0;
+	local garpia_bonus = 0;
+	local pirate_bonus = 0;
 	local base_bonus = 0.3;
 
 	for _, player in ipairs( onlinePlayers ) do
@@ -28,6 +32,12 @@ local function sendTavernBonusesTheramore()
 				end
 				if (player:HasAura(aura_ratusha)) then
 					ratusha_bonus = ratusha_bonus + base_bonus;
+				end
+				if (player:HasAura(aura_garpia)) then
+					garpia_bonus = garpia_bonus + base_bonus;
+				end
+				if (player:HasAura(aura_pirate)) then
+					pirate_bonus = pirate_bonus + base_bonus;
 				end
 			end
 		end;
@@ -48,6 +58,14 @@ local function sendTavernBonusesTheramore()
 	if (ratusha_bonus > 0) then
 		local ratusha_bonus_ceil = math.ceil(ratusha_bonus)
 		SendMail('Казна ратуши', 'Бонус за активность ратуши', 98676, 36, 61, 20, 0, 0, 301396, ratusha_bonus_ceil); -- 98676 персонаж Джайна (Розальба)
+	end
+	if (garpia_bonus > 0) then
+		local garpia_bonus_ceil = math.ceil(garpia_bonus)
+		SendMail('Доход Мертвой Гарпии', 'Бонус за активность Мертвой Гарпии', 2997, 36, 61, 20, 0, 0, 301396, garpia_bonus_ceil); -- 2997 персонаж Гук
+	end
+	if (pirate_bonus > 0) then
+		local pirate_bonus_ceil = math.ceil(pirate_bonus)
+		SendMail('Дозод трактира', 'Бонус за активность трактира', 22059, 36, 61, 20, 0, 0, 301396, pirate_bonus_ceil); -- 22059 персонаж Оттар
 	end
 end
 CreateLuaEvent( sendTavernBonusesTheramore, 1800000, 0 )
