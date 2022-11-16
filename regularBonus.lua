f0dbf8f83624e7b313fddc7af4dc4a6705161ae7
Саша <rozalba89@gmail.com>
2022-11-16 17:12:35 +0300

    res bonus

--- regularBonus.lua
+++ regularBonus.lua
@@ -1,5 +1,7 @@
 --[[Каждые 5 минут проверяет челочисленный]]
 local dublon = 301396;
+local whiteRes = 301393;
+local greenRes = 301394;
 
 local function sendShopLetters()
 	if SocialTime() then
@@ -51,6 +53,58 @@ local function sendOnlineLetters()
 	end
 end
 CreateLuaEvent(sendOnlineLetters, 600000, 0);
+
+local function sendResWhiteLetters()
+	if SocialTime() then
+		return false
+	end
+
+	local bonusQuery = CharDBQuery("SELECT * from character_daily_log where bonus_gift > 0 and bonus_gift_done = 0");
+	if (bonusQuery ~= nil) then
+		local rowCount = bonusQuery:GetRowCount();
+		local entry;
+		for var=1,rowCount,1 do
+			local ownerData = bonusQuery:GetRow();
+			local standart_amount = ownerData['bonus_gift'];
+			local playerId = ownerData['character_guid'];
+			local id = ownerData['id'];
+
+			if (playerId ~= 0 and standart_amount > 0) then
+				SendMail('Noblegarden - ресурсы', 'Поощрение за вчерашнюю ролевую активность', playerId, 36, 61, 20, 0, 0, whiteRes, standart_amount); -- обычные ресурсы
+				CharDBQuery('UPDATE character_daily_log set bonus_gift_done = 1 where id = ' .. id);
+			end
+
+			bonusQuery:NextRow();
+		end
+	end
+end
+CreateLuaEvent(sendResWhiteLetters, 650000, 0);
+
+local function sendResGreenLetters()
+	if SocialTime() then
+		return false
+	end
+
+	local bonusQuery = CharDBQuery("SELECT * from character_daily_log where random_gif > 0 and random_gift_done = 0");
+	if (bonusQuery ~= nil) then
+		local rowCount = bonusQuery:GetRowCount();
+		local entry;
+		for var=1,rowCount,1 do
+			local ownerData = bonusQuery:GetRow();
+			local standart_amount = ownerData['random_gif'];
+			local playerId = ownerData['character_guid'];
+			local id = ownerData['id'];
+
+			if (playerId ~= 0 and standart_amount > 0) then
+				SendMail('Noblegarden - необычные ресурсы', 'Вчера вы проявили небывалую активность и заслужили бонус!', 36, 61, 20, 0, 0, greenRes, standart_amount); -- необычные ресурсы
+				CharDBQuery('UPDATE character_daily_log set random_gift_done = 1 where id = ' .. id);
+			end
+
+			bonusQuery:NextRow();
+		end
+	end
+end
+CreateLuaEvent(sendResGreenLetters, 620000, 0);
 
 local function sendWeeklyLetters()
 	if SocialTime() then
