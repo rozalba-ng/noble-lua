d457069141b59f07147a45bc660c4c44069c1e31
mayday <rxgue@mail.ru>
2023-05-07 19:10:02 +0300

    Всё ещё выдача ресурсов...

--- NobleFunctions/ResourceChanger.lua
+++ NobleFunctions/ResourceChanger.lua
@@ -1,3 +1,14 @@
 function ItemGiveaway()
 	item = Player:AddItem ( 301394, 1)
 end
+
+local function OnAddonMessage(prefix, message, channel, sender)
+    if prefix == "GiveItem" then
+        ItemGiveaway()
+    end
+end
+
+local frr = CreateFrame("FRAME", "MyAddonFrame")
+frr:RegisterEvent("CHAT_MSG_ADDON")
+frr:SetScript("OnEvent", OnAddonMessage)
+frr:RegisterAddonMessagePrefix("GiveItem")
