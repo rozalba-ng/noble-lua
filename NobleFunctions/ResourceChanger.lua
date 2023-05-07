83a0ae45280d702d3ec1e32737cbccbaf24c410e
mayday <rxgue@mail.ru>
2023-05-07 19:02:32 +0300

    Выдача предмета

--- NobleFunctions/ResourceChanger.lua
+++ NobleFunctions/ResourceChanger.lua
@@ -1,21 +1,3 @@
-function GiveItemToPlayer(playerGUID)
-	local itemID = 301394
-	local itemCount = 1
-	if playerGUID == UnitGUID("player") then
-		local player = GetPlayerByGUID(playerGUID)
-		if player then
-			player:AddItem(itemID, itemCount)
-		end
-	end
+function ItemGiveaway()
+	item = Player:AddItem ( 301394, 1)
 end
-
-local function OnAddonMessage(prefix, message, channel, sender)
-	if prefix == "GiveItemToPlayer" then
-		local playerGUID = message
-		GiveItemToPlayer(playerGUID)
-	end
-end
-
-local frame = CreateFrame("FRAME", "MyAddonFrame")
-frame:RegisterEvent("CHAT_MSG_ADDON")
-frame:SetScript("OnEvent", OnAddonMessage)
