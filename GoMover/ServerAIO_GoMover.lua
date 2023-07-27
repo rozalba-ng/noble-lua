0ea7bf313325430898b11af121502712b52e7ead
Саша <rozalba89@gmail.com>
2023-07-28 01:07:04 +0300

    gather and mail to players

--- GoMover/ServerAIO_GoMover.lua
+++ GoMover/ServerAIO_GoMover.lua
@@ -1,162 +1,163 @@
-
 local AIO = AIO or require("AIO")
 
 local GoMover = AIO.AddHandlers("GOM_Handlers", {})
 
 playersCooldowns = {}
 
-function copysign(value,valuetocopy)
-	return value * (valuetocopy / math.abs(valuetocopy))
+function copysign(value, valuetocopy)
+    return value * (valuetocopy / math.abs(valuetocopy))
 end
 
 
-function FromQuatToEuler(x,y,z,w)
+function FromQuatToEuler(x, y, z, w)
 
-	local sinr_cosp = 2 * (w * x + y * z);
+    local sinr_cosp = 2 * (w * x + y * z);
     local cosr_cosp = 1 - 2 * (x * x + y * y);
     local roll = math.atan2(sinr_cosp, cosr_cosp);
     local sinp = 2 * (w * y - z * x);
-	local pitch = 0
+    local pitch = 0
     if (math.abs(sinp) >= 1) then
         pitch = copysign(math.pi() / 2, sinp)
     else
         pitch = math.asin(sinp);
-	end
+    end
     local siny_cosp = 2 * (w * z + x * y);
     local cosy_cosp = 1 - 2 * (y * y + z * z);
     local yaw = math.atan2(siny_cosp, cosy_cosp);
-	return yaw , pitch ,roll
+    return yaw, pitch, roll
 end
 
-function GoMover.StartRotate(player,guid,value,rotateType)
-	if ( not playersCooldowns[player:GetName()] or (  os.time() - playersCooldowns[player:GetName()]  ) > 0.3 )  then
-		playersCooldowns[player:GetName()] = os.time()
-		q = WorldDBQuery('SELECT rotation0, rotation1,rotation2,rotation3 FROM gameobject WHERE guid ='..guid)	
-		local x,y,z = FromQuatToEuler(q:GetFloat(0),q:GetFloat(1),q:GetFloat(2),q:GetFloat(3))
-		AIO.Handle(player,"GOM_Handlers","RotateGo",rotateType,x,y,z,value)
-	end
+function GoMover.StartRotate(player, guid, value, rotateType)
+    if (not playersCooldowns[player:GetName()] or (os.time() - playersCooldowns[player:GetName()]) > 0.3) then
+        playersCooldowns[player:GetName()] = os.time()
+        q = WorldDBQuery('SELECT rotation0, rotation1,rotation2,rotation3 FROM gameobject WHERE guid =' .. guid)
+        local x, y, z = FromQuatToEuler(q:GetFloat(0), q:GetFloat(1), q:GetFloat(2), q:GetFloat(3))
+        AIO.Handle(player, "GOM_Handlers", "RotateGo", rotateType, x, y, z, value)
+    end
 end
-function GoMover.StartMove(player,guid,value,moveType)
-	if ( not playersCooldowns[player:GetName()] or (  os.time() - playersCooldowns[player:GetName()]  ) > 0.3 )  then
-		playersCooldowns[player:GetName()] = os.time()
-		q = WorldDBQuery('SELECT position_x, position_y,position_z,orientation FROM gameobject WHERE guid ='..guid)
-		local x,y,z,o = q:GetFloat(0),q:GetFloat(1),q:GetFloat(2),q:GetFloat(3)
-		local po = player:GetO()
-		AIO.Handle(player,"GOM_Handlers","MoveGo",moveType,x,y,z,po,value)
-	end
-end
-function GoMover.StartScale(player,guid,value,scaleType)
-	if ( not playersCooldowns[player:GetName()] or (  os.time() - playersCooldowns[player:GetName()]  ) > 0.3 )  then
-		playersCooldowns[player:GetName()] = os.time()
-		AIO.Handle(player,"GOM_Handlers","ScaleGo",scaleType,value)
-	end
-end
-function GoMover.ReturnToInventory(player,guid)
-	player:SendBroadcastMessage("Для того чтобы забрать объект воспользуйтесь кнопкой в главном меню режима строительства")
-end
-function GOM_OpenEditAddon(player,gob)
-	AIO.Handle(player,"GOM_Handlers","SetName",gob:GetName())
-	AIO.Handle(player,"GOM_Handlers","GetGUID",gob:GetDBTableGUIDLow())
 
+function GoMover.StartMove(player, guid, value, moveType)
+    if (not playersCooldowns[player:GetName()] or (os.time() - playersCooldowns[player:GetName()]) > 0.3) then
+        playersCooldowns[player:GetName()] = os.time()
+        q = WorldDBQuery('SELECT position_x, position_y,position_z,orientation FROM gameobject WHERE guid =' .. guid)
+        local x, y, z, o = q:GetFloat(0), q:GetFloat(1), q:GetFloat(2), q:GetFloat(3)
+        local po = player:GetO()
+        AIO.Handle(player, "GOM_Handlers", "MoveGo", moveType, x, y, z, po, value)
+    end
 end
-local function GatherGosByRadius(player,radius)
-	if radius <= 10 then
-		local gos = player:GetGameObjectsInRange(tonumber(radius))
-		if(player:GetGMRank() == 2 or player:GetGMRank() == 1) then
-			player:Print("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFДоступ запрещен для вашего типа аккаунта.|r")
-			return false
-		end
-		for i, gob in pairs(gos) do
-			if gob:GetOwner() == player and gob:GetPhaseMask() ~= 1024 then
-				local map = player:GetMap()
-				local entry = gob:GetEntry()
-				local item = player:AddItem(entry)
-				if(item == nil)then
-					player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет места.|r")
-					return false
-				end
-				gob:RemoveFromWorld(true)
-			end
-		
-		
-		end
-	else
-		player:Print("Некорректно задан радиус сбора игровых объектов")
-	end
+
+function GoMover.StartScale(player, guid, value, scaleType)
+    if (not playersCooldowns[player:GetName()] or (os.time() - playersCooldowns[player:GetName()]) > 0.3) then
+        playersCooldowns[player:GetName()] = os.time()
+        AIO.Handle(player, "GOM_Handlers", "ScaleGo", scaleType, value)
+    end
+end
+
+function GoMover.ReturnToInventory(player, guid)
+    player:SendBroadcastMessage("Для того чтобы забрать объект воспользуйтесь кнопкой в главном меню режима строительства")
+end
+
+function GOM_OpenEditAddon(player, gob)
+    AIO.Handle(player, "GOM_Handlers", "SetName", gob:GetName())
+    AIO.Handle(player, "GOM_Handlers", "GetGUID", gob:GetDBTableGUIDLow())
+end
+
+local function GatherGosByRadius(player, radius)
+    if radius <= 10 then
+        local gos = player:GetGameObjectsInRange(tonumber(radius))
+        if (player:GetGMRank() == 2 or player:GetGMRank() == 1) then
+            player:Print("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFДоступ запрещен для вашего типа аккаунта.|r")
+            return false
+        end
+        for i, gob in pairs(gos) do
+            if gob:GetOwner() == player and gob:GetPhaseMask() ~= 1024 then
+                local map = player:GetMap()
+                local entry = gob:GetEntry()
+                local item = player:AddItem(entry)
+                if (item == nil) then
+                    player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет места.|r")
+                    return false
+                end
+                gob:RemoveFromWorld(true)
+            end
+        end
+    else
+        player:Print("Некорректно задан радиус сбора игровых объектов")
+    end
 end
 
 local function ReturnGosByRadius(player, radius)
-	if radius <= 10 then
-		local gos = player:GetGameObjectsInRange(tonumber(radius))
-		if not (player:GetGMRank() > 1 or player:GetDmLevel() == 5 ) then
-			player:Print("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFДоступ запрещен для вашего типа аккаунта.|r")
-			return false
-		end
+    if radius <= 10 then
+        local gos = player:GetGameObjectsInRange(tonumber(radius))
+        if not (player:GetGMRank() > 1 or player:GetDmLevel() == 5) then
+            player:Print("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFДоступ запрещен для вашего типа аккаунта.|r")
+            return false
+        end
 
-		local ownerIDs = {}
-		for i, gob in pairs(gos) do
-			local ownerId = gob:GetOwnerId()
-			if gob:GetPhaseMask() == 1 or ownerId == 0 then
-				table.insert(ownerIDs, ownerId)
-			end
-		end
-		if #ownerIDs == 0 then
-			player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет объектов, которые можно вернуть пользователям.|r")
-			return
-		end
-		local owners = table.concat(ownerIDs,", ")
-		local ownersList = {}
-		local Q = CharDBQuery("SELECT c.guid FROM characters.characters c WHERE c.guid in ("..owners..") and c.account not in (select a.id from auth.account_access a where 1")
-		if Q then
-			for i = 1, Q:GetRowCount() do
-				local c_guid = Q:GetUInt32(0)
-				ownersList[c_guid] = true
-				Q:NextRow()
-			end
-		else
-			player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет объектов, которые можно вернуть пользователям.|r")
-			return
-		end
-		if #ownersList == 0 then
-			player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет объектов, которые можно вернуть пользователям.|r")
-			return
-		end
+        local ownerIDs = {}
+        for i, gob in pairs(gos) do
+            local ownerId = gob:GetOwnerId()
+            if gob:GetPhaseMask() == 1 and ownerId ~= 0 then
+                table.insert(ownerIDs, tostring(ownerId))
+            end
+        end
+        if #ownerIDs == 0 then
+            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет объектов, которые можно вернуть пользователям.|r")
+            return
+        end
+        local owners = table.concat(ownerIDs, ", ")
+        local ownersList = {}
+        local Q = CharDBQuery("SELECT c.guid FROM characters.characters c WHERE c.guid in (" .. owners .. ") and c.account not in (select a.id from auth.account_access a where 1")
+        if Q then
+            for i = 1, Q:GetRowCount() do
+                local c_guid = Q:GetUInt32(0)
+                ownersList[c_guid] = true
+                Q:NextRow()
+            end
+        else
+            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет объектов, которые можно вернуть пользователям.|r")
+            return
+        end
+        if #ownersList == 0 then
+            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНет объектов, которые можно вернуть пользователям.|r")
+            return
+        end
 
-		for i, gob in pairs(gos) do
-			if gob:GetPhaseMask() == 1 and ownersList[gob:GetOwnerId()] ~= nil then
-				local entry = gob:GetEntry()
-				local itemGUIDlow = SendMail('Сервер', 'Возврат имущества: переносной объект', gob:GetOwnerId(), 36, 61, 20, 0, 0, entry, 1);
-				local item = player:AddItem(entry)
-				if (itemGUIDlow == nil) then
-					player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНе удалось отправить объект " .. entry .. ".|r")
-					return false
-				end
-				gob:RemoveFromWorld(true)
-			end
-		end
-	else
-		player:Print("Некорректно задан радиус сбора игровых объектов")
-	end
+        for i, gob in pairs(gos) do
+            if gob:GetPhaseMask() == 1 and ownersList[gob:GetOwnerId()] ~= nil then
+                local entry = gob:GetEntry()
+                local itemGUIDlow = SendMail('Сервер', 'Возврат имущества: переносной объект', gob:GetOwnerId(), 36, 61, 20, 0, 0, entry, 1);
+                local item = player:AddItem(entry)
+                if (itemGUIDlow == nil) then
+                    player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНе удалось отправить объект " .. entry .. ".|r")
+                    return false
+                end
+                gob:RemoveFromWorld(true)
+            end
+        end
+    else
+        player:Print("Некорректно задан радиус сбора игровых объектов")
+    end
 end
 
 local function OnPlayerCommandWithArg(event, player, code)
-    if(code == "movego")then
-		local nearestGo = player:GetNearestGameObject(10)
-		if nearestGo then
-			if (nearestGo:GetOwner() == player) or player:GetGMRank() > 0 then
-				AIO.Handle(player,"GOM_Handlers","SetName",nearestGo:GetName())
-				AIO.Handle(player,"GOM_Handlers","GetGUID",nearestGo:GetDBTableGUIDLow())
-			else
-				player:SendBroadcastMessage("Рядом стоящий объект вам не принадлежит")
-			end
-		else
-			player:SendBroadcastMessage("Объектов в радиусе не было обнаружено")
-		end
-	elseif(code == "gathergos")then
-		GatherGosByRadius(player,5)
-	elseif(code == "returngos")then
-		ReturnGosByRadius(player,5)
-	end
+    if (code == "movego") then
+        local nearestGo = player:GetNearestGameObject(10)
+        if nearestGo then
+            if (nearestGo:GetOwner() == player) or player:GetGMRank() > 0 then
+                AIO.Handle(player, "GOM_Handlers", "SetName", nearestGo:GetName())
+                AIO.Handle(player, "GOM_Handlers", "GetGUID", nearestGo:GetDBTableGUIDLow())
+            else
+                player:SendBroadcastMessage("Рядом стоящий объект вам не принадлежит")
+            end
+        else
+            player:SendBroadcastMessage("Объектов в радиусе не было обнаружено")
+        end
+    elseif (code == "gathergos") then
+        GatherGosByRadius(player, 5)
+    elseif (code == "returngos") then
+        ReturnGosByRadius(player, 5)
+    end
 end
 
 
