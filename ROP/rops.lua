9a9ebe942c1e676f78f0533ae9466f7725155673
mayday <rxgue@mail.ru>
2023-08-26 20:30:04 +0300

    Всё ещё вывод ропов

--- ROP/rops.lua
+++ ROP/rops.lua
@@ -4,7 +4,7 @@ local GET_ROPS = "SELECT id, char_id, title, nop FROM character_nops"
 local GET_CHAR_ID = "SELECT guid FROM characters"
 
 function TestRop()
-	local result = queryDatabase(GET_ROPS)
+	local results = queryDatabase(GET_ROPS)
 	
 	if results then
         for _, row in ipairs(results) do
