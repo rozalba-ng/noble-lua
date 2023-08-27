ac9abf553cb12495b3df57b7be781742196f6dab
mayday <rxgue@mail.ru>
2023-08-28 00:32:37 +0300

    Почти идеальные РОПы и тест новых чантов

--- ROP/rops.lua
+++ ROP/rops.lua
@@ -1,23 +1,55 @@
--- Разобраться с AIO
 local AIO = AIO or require("AIO")
-local ROPHandlers = AIO.AddHandlers("ROPHandlers", {})
+
+local ROPHandler = AIO.AddHandlers("ROPHandler", {})
 
 local GET_INFO = "SELECT character_nops.id, character_nops.char_id, character_nops.title, character_nops.nop FROM character_nops LEFT JOIN characters ON character_nops.char_id = characters.guid WHERE characters.guid = "
 
-function TestRop(player)
-	local playerGuid = player:GetGUIDLow()
-    local result = CharDBQuery(GET_INFO ..playerGuid)
+function SendTargetROPs(target)
+    local targetGuid = target:GetGUIDLow()
+    local result = CharDBQuery(GET_INFO ..targetGuid)
+	local rowCount = result:GetRowCount()
     if result then
-		
-		local id = result:GetUInt32(0)
-		local char_id = result:GetUInt32(1)
-		local title = result:GetString(2)
-		local nop = result:GetString(3)
-	
-		player:SendBroadcastMessage(id)
-		player:SendBroadcastMessage(char_id)
-		player:SendBroadcastMessage(title)
-		player:SendBroadcastMessage(nop)
+		local ropsd = ropsd or {}
+		for i = 1, rowCount do		
+			local id = result:GetUInt32(0) or 0
+			local title = result:GetString(2) or "error"
+			local nop = result:GetString(3) or "error"
+			table.insert(ropsd, {title = title, id = id, nop = nop})
+			i = i + 1
+			--[[ player:SendBroadcastMessage(tostring(rops)) ]]
+			result:NextRow()
+		end
+        AIO.Handle(player, "ROPHandler", "SendTargetROPInfo", {
+            --[[ title = title;
+            nop = nop; ]]
+			ropsd = ropsd
+        })
+    else
+        player:SendBroadcastMessage("У персонажа нет РОПов!")
+    end
+end
+
+function SendROPs(player)
+    local playerGuid = player:GetGUIDLow()
+    local result = CharDBQuery(GET_INFO ..playerGuid)
+	local rowCount = result:GetRowCount()
+	print(result[1])
+    if result then
+		local rops = rops or {}
+		for i = 1, rowCount do		
+			local id = result:GetUInt32(0) or 0
+			local title = result:GetString(2) or "error"
+			local nop = result:GetString(3) or "error"
+			table.insert(rops, {title = title, id = id, nop = nop})
+			i = i + 1
+			--[[ player:SendBroadcastMessage(tostring(rops)) ]]
+			result:NextRow()
+		end
+        AIO.Handle(player, "ROPHandler", "SendROPInfo", {
+            --[[ title = title;
+            nop = nop; ]]
+			rops = rops
+        })
     else
         player:SendBroadcastMessage("У персонажа нет РОПов!")
     end
@@ -25,7 +57,8 @@ end
 
 local function OnCommand(event, player, command)
     if command == "rops" then
-        TestRop(player)
+        SendROPs(player)
+		SendTargetROPs(target)
     end
 end
 
