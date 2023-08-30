76d8fde9b190500d00b35a4bd5d8d11ce03d53fb
mayday <rxgue@mail.ru>
2023-08-30 22:28:15 +0300

    rops done

--- ROP/rops.lua
+++ ROP/rops.lua
@@ -2,27 +2,32 @@ local AIO = AIO or require("AIO")
 
 local ROPHandler = AIO.AddHandlers("ROPHandler", {})
 
-local GET_INFO = "SELECT character_nops.id, character_nops.char_id, character_nops.title, character_nops.nop FROM character_nops LEFT JOIN characters ON character_nops.char_id = characters.guid WHERE characters.guid = "
+local GET_INFO = "SELECT character_nops.id, character_nops.title, character_nops.nop, character_nops.icon FROM character_nops LEFT JOIN characters ON character_nops.char_id = characters.guid WHERE characters.guid = "
 
 function SendTargetROPs(player)
-	local target = player:GetSelection()
-    local targetGuid = target:GetGUIDLow()
-    local result = CharDBQuery(GET_INFO ..targetGuid)
-	local rowCount = result:GetRowCount()
-    if result then
-		local rops2 = rops2 or {}
-		for i = 1, rowCount do		
-			local id = result:GetUInt32(0) or 0
-			local title = result:GetString(2) or "error"
-			local nop = result:GetString(3) or "error"
-			table.insert(rops2, {title = title, id = id, nop = nop})
-			result:NextRow()
-		end
-        AIO.Handle(player, "ROPHandler", "SendTargetROPInfo", {
-			rops2 = rops2
-        })
-    else
+    local target = player:GetSelection()
+    if not target then
         return
+    end
+
+    local targetGuid = target:GetGUIDLow()
+    local result = CharDBQuery(GET_INFO .. targetGuid)
+
+    if result then
+        local rops2 = {}
+
+        local rowCount = result:GetRowCount()
+        for i = 1, rowCount do
+            local title = result:GetString(1) or "error"
+            local nop = result:GetString(2) or "error"
+            local icon = result:GetString(3) or "error"
+            table.insert(rops2, { title = title, nop = nop, icon = icon })
+            result:NextRow()
+        end
+
+        AIO.Handle(player, "ROPHandler", "SendTargetROPInfo", {
+            rops2 = rops2
+        })
     end
 end
 
@@ -32,12 +37,11 @@ function SendROPs(player)
 	local rowCount = result:GetRowCount()
     if result then
 		local rops = rops or {}
-		for i = 1, rowCount do		
-			local id = result:GetUInt32(0) or 0
-			local title = result:GetString(2) or "error"
-			local nop = result:GetString(3) or "error"
-			table.insert(rops, {title = title, id = id, nop = nop})
-			i = i + 1
+		for i = 1, rowCount do
+			local title = result:GetString(1) or "error"
+			local nop = result:GetString(2) or "error"
+			local icon = result:GetString(3) or "error"
+			table.insert(rops, {title = title, nop = nop, icon = icon})
 			result:NextRow()
 		end
         AIO.Handle(player, "ROPHandler", "SendROPInfo", {
@@ -55,12 +59,6 @@ local function OnCommand(event, player, command)
 end
 
 RegisterPlayerEvent(42, OnCommand)
-
-local function OnLogin(event, player)
-    SendROPs(player)
-end
-
-RegisterPlayerEvent(3, OnLogin)
 
 local function OnTargetCommand(event, player, command)
     if command == "ropsd" then
