0b2554f95f3e57ef50a8fd9eb14ed06bc1895f20
mayday <rxgue@mail.ru>
2024-01-26 19:39:51 +0300

    typing buff command

--- typingBuff.lua
+++ typingBuff.lua
@@ -11,32 +11,34 @@ function RemoveBuff(player, buffId)
 end
 
 function ChatBuffs(event, player, command, message)
-    local cmd, arg = string.match(message, "^%.(%w+)%s*(%d*)$")
-    if cmd == "typingsay" then
-        if arg == "1" then
-            ApplyBuff(player, SayBuffId)
-        elseif arg == "0" then
-            RemoveBuff(player, SayBuffId)
-        else
-            player:SendBroadcastMessage("Неверный формат. Попробуйте .typingsay [1/0]")
-        end
-    elseif cmd == "typingyell" then
-        if arg == "1" then
-            ApplyBuff(player, YellBuffId)
-        elseif arg == "0" then
-            RemoveBuff(player, YellBuffId)
-        else
-            player:SendBroadcastMessage("Неверный формат. Попробуйте .typingyell [1/0]")
-        end
-    elseif cmd == "typingemote" then
-        if arg == "1" then
-            ApplyBuff(player, EmoteBuffId)
-        elseif arg == "0" then
-            RemoveBuff(player, EmoteBuffId)
-        else
-            player:SendBroadcastMessage("Неверный формат. Попробуйте .typingemote [1/0]")
-        end
-    end
+    if string.find(command, " ") then
+        local _, _, cmd, arg = string.find(command, "(%S+)%s+(.*)")
+		if cmd == "typingsay" then
+			if arg == "1" then
+				ApplyBuff(player, SayBuffId)
+			elseif arg == "0" then
+				RemoveBuff(player, SayBuffId)
+			else
+				player:SendBroadcastMessage("Неверный формат. Попробуйте .typingsay [1/0]")
+			end
+		elseif cmd == "typingyell" then
+			if arg == "1" then
+				ApplyBuff(player, YellBuffId)
+			elseif arg == "0" then
+				RemoveBuff(player, YellBuffId)
+			else
+				player:SendBroadcastMessage("Неверный формат. Попробуйте .typingyell [1/0]")
+			end
+		elseif cmd == "typingemote" then
+			if arg == "1" then
+				ApplyBuff(player, EmoteBuffId)
+			elseif arg == "0" then
+				RemoveBuff(player, EmoteBuffId)
+			else
+				player:SendBroadcastMessage("Неверный формат. Попробуйте .typingemote [1/0]")
+			end
+		end
+	end
 end
 
 RegisterPlayerEvent(42, ChatBuffs)
