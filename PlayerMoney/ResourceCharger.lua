e22203ce3fb01608e6f3cf22a1a969b82f91a275
Саша <rozalba89@gmail.com>
2024-04-17 12:16:41 +0300

    ресы - убираю покпку необычных

--- PlayerMoney/ResourceCharger.lua
+++ PlayerMoney/ResourceCharger.lua
@@ -7,40 +7,19 @@ local MenuId = 61265;
 
 local function OnGossipChargerRes(event, player, object)
     player:GossipClearMenu() -- required for player gossip
-    player:GossipMenuAddItem(0, "КУПИТЬ необычные ресурсы (максимум 5 за раз, вводить количество в поле <код>)", 1, 2, true, nil)
     player:GossipMenuAddItem(0, "ПРОДАТЬ необычные ресурсы (максимум 5 за раз, вводить количество в поле <код>))", 1, 3, true, nil)
     player:GossipMenuAddItem(0, "Закрыть", 1, 5)
-    player:GossipSetText( 'ООС: Тут вы можете обменять обычные ресурсы на необычные и обратно. Необычные ресурсы требуются для заказа рисовок необычного качества.\n\n После обмена ресурсы придут на почту. \n\nКурс покупки и продажи: 10 обычных за 1 необычный', MenuId )
+    player:GossipSetText( 'ООС: Тут вы можете обменять необычные ресурсы на обычные.\n\n После обмена ресурсы придут на почту. \n\nКурс покупки и продажи: 10 обычных за 1 необычный', MenuId )
     player:GossipSendMenu(MenuId, object, MenuId) -- MenuId required for player gossip
 end
 
 local function OnGossipChargerSelectRes(event, player, object, sender, intid, code, menuid)
     local level = player:GetNobleLevel()
 
-    if (intid == 2) then
+    if (intid == 3) then
         local num = tonumber(code);
         if (num > 5) then
-            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFОШИБКА! Нельзя покупать или продавать более 5 необычных ресурсов за раз |r");
-            player:GossipComplete()
-            return false;
-        end
-
-        local whiteResursNeeded;
-        whiteResursNeeded = num*10;
-
-        if (player:HasItem(whiteResurs, whiteResursNeeded)) then
-            SendMail( "Обмен у кладовщика", "Приятной игры", player:GetGUIDLow(), 0, 61, 0, 0, 0, greenResurs, num )
-            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFОбмен проведен успешно! Выслано необычных ресурсов: |r" .. tostring(num));
-            player:RemoveItem(whiteResurs, whiteResursNeeded);
-            player:GossipComplete()
-        else
-            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFНедостаточно обычных ресурсов для покупки нужного числа необычных ресурсов. Требуемое число обычных ресурсов: |r" .. tostring(whiteResursNeeded));
-            player:GossipComplete()
-        end
-    elseif (intid == 3) then
-        local num = tonumber(code);
-        if (num > 5) then
-            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFОШИБКА! Нельзя покупать или продавать более 5 необычных ресурсов за раз |r");
+            player:SendBroadcastMessage("|cFF00CC99|r |cFFFFA500System: |r |cFF00CCFFОШИБКА! Нельзя продавать более 5 необычных ресурсов за раз |r");
             player:GossipComplete();
             return false;
         end
